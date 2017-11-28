2ea40aa64cd863bbd5bbb9ddae6f5f92ea1178e6
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2017-11-28 14:44:59 -0600

    .

---
+++ src/main/java/com/sitemascompras/encriptacion/ControladorEncriptacion.java
@@ -0,0 +1,10 @@
+package com.sitemascompras.encriptacion;
+
+public class ControladorEncriptacion {
+	private final String KEY_EXTENSION = ".key";
+	private final String PUBLIC = "public";
+	private final String PRIVATE = "private";
+	private final String MESSAGE_ENCRYPT_EXTENSION = ".encript";
+	private final String PATH = "C:/encrpitacion/";
+
+}
