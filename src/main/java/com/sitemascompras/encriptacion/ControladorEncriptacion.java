b4a2b6c022b626ec0de9e61b43de8b3a945536c5
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2017-11-30 17:15:25 -0600

    .

--- src/main/java/com/sitemascompras/encriptacion/ControladorEncriptacion.java
+++ src/main/java/com/sitemascompras/encriptacion/ControladorEncriptacion.java
@@ -1,50 +1,152 @@
 package com.sitemascompras.encriptacion;
 
+<<<<<<< HEAD
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+=======
+>>>>>>> e22e5a1165cc42a7170a6d123de82f94844a4856
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class ControladorEncriptacion {
 	private final String KEY_EXTENSION = ".key";
-	private final String PUBLIC = "public.key";
-	private final String PRIVATE = "private.key";
+	private final String PUBLIC = "public";
+	private final String PRIVATE = "private";
 	private final String MESSAGE_ENCRYPT_EXTENSION = ".encript";
-	//private final String PATH = "C:/encrpitacion/";
-	// Estas son las rutas donde se van a guardar las llaves esto es temporal porque tambien se 
-	// se puden almacenar  en la base de datos.
-	private final String PRIVATEKEYPATH = "C:/encrpitacion/llavesPrivadas";
-	private final String PUBLICKEYPATH = "C:/encrpitacion/llavesPublicas";
+	private final String MESSAGE_ENCRYPT_PATH = "C:/encriptacion/mensajesEncriptados";
+	private final String PRIVATE_KEY_PATH = "C:/encrpitacion/llavesPrivadas";
+	private final String PUBLIC_KEY_PATH = "C:/encrpitacion/llavesPublicas";
 	
 	
 	
-	public void crearLlaves(String nombreLlave) throws InvalidKeySpecException, NoSuchAlgorithmException {
-		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-		KeyFactory fact = KeyFactory.getInstance("RSA");
-		kpg.initialize(2048);
-		KeyPair kp = kpg.genKeyPair();
-		RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(),
-		  RSAPublicKeySpec.class);
-		RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(),
-		  RSAPrivateKeySpec.class);
+	public void crearLlaves(String nombreLlave) throws Exception {
+		
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			kpg.initialize(2048);
+			KeyPair kp = kpg.genKeyPair();
+			RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(),
+			  RSAPublicKeySpec.class);
+			RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(),
+			  RSAPrivateKeySpec.class);
+	
+			saveToFile(PUBLIC_KEY_PATH + nombreLlave+"public.key", pub.getModulus(),
+			  pub.getPublicExponent());
+			saveToFile(PRIVATE_KEY_PATH + nombreLlave+"private.key", priv.getModulus(),
+			  priv.getPrivateExponent());
+		
+	}
+	
+	
+	public void saveToFile(String fileName,BigInteger mod, BigInteger exp) throws IOException {
+		
+			ObjectOutputStream oout = new ObjectOutputStream(
+				    new BufferedOutputStream(new FileOutputStream(fileName)));
+			try {
+				oout.writeObject(mod);
+				oout.writeObject(exp);
+			} catch (Exception e) {
+				throw new IOException("Unexpected error", e);
+			} finally {
+			    oout.close();
+			}
+		
+		
+	}
 
-		guardarLlave(PUBLICKEYPATH + nombreLlave+PUBLIC, pub.getModulus(),
-		  pub.getPublicExponent());
-		guardarLlave(PRIVATEKEYPATH + nombreLlave+PRIVATE, priv.getModulus(),
-		  priv.getPrivateExponent());
+	public void encryptMessage(String messageName, String message, String keyName) throws Exception {
+		
+		PublicKey pubKey = (PublicKey)readKeyFromFile(keyName, PUBLIC);
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+		byte[] encryptedData = cipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
+	    Encoder oneEncoder = Base64.getEncoder();
+	    encryptedData = oneEncoder.encode(encryptedData);
+		writeBytesFile(messageName,encryptedData,MESSAGE_ENCRYPT_EXTENSION);
+
+	}
+
+
+	private void writeBytesFile(String name, byte[] content, String type) throws FileNotFoundException, IOException{
+		
+		FileOutputStream fos = new FileOutputStream(MESSAGE_ENCRYPT_PATH + name + type);
+		fos.write(content);
+		fos.close();
+		
+	}
+
+
+	Key readKeyFromFile(String keyFileName, String type) throws IOException {
+		
+		 InputStream in = new FileInputStream (PUBLIC_KEY_PATH + keyFileName+ type + KEY_EXTENSION);
+		  ObjectInputStream oin = new ObjectInputStream(new BufferedInputStream(in));
+		  try {
+		    BigInteger m = (BigInteger) oin.readObject();
+		    BigInteger e = (BigInteger) oin.readObject();
+		    if (type.equals("public")) {
+			    RSAPublicKeySpec keySpec = new RSAPublicKeySpec(m, e);
+			    KeyFactory fact = KeyFactory.getInstance("RSA");
+			    PublicKey pubKey = fact.generatePublic(keySpec);
+			    return pubKey;		    	
+		    } else {
+		    	RSAPrivateKeySpec keySpec = new RSAPrivateKeySpec(m, e);
+			    KeyFactory fact = KeyFactory.getInstance("RSA");
+			    PrivateKey pubKey = fact.generatePrivate(keySpec);
+			    return pubKey;		    	
+		    }
+		  } catch (Exception e) {
+		    throw new RuntimeException("Spurious serialisation error", e);
+		  } finally {
+		    oin.close();
+		  }
+		  
+		  
+	}
+	
+<<<<<<< HEAD
+	private byte[] readMessageFile(String messageName) throws Exception{
+		File file = new File(MESSAGE_ENCRYPT_PATH + messageName + MESSAGE_ENCRYPT_EXTENSION);
+        int length = (int) file.length();
+        BufferedInputStream reader = new BufferedInputStream(new FileInputStream(file));
+        byte[] bytes = new byte[length];
+        reader.read(bytes, 0, length);
+        reader.close();
+        Decoder oneDecoder = Base64.getDecoder();
+		return oneDecoder.decode(bytes);
 		
 	}
 	
-	
-	public void guardarLlave(String string, BigInteger bigInteger, BigInteger bigInteger2) {
-		
-	}
-	
+
+}// llave de la clase
+
+
+
+
+=======
 }
-
-
+>>>>>>> e22e5a1165cc42a7170a6d123de82f94844a4856
---
+++ src/main/java/com/sitemascompras/encriptacion/Firma.java
@@ -0,0 +1,22 @@
+package com.sitemascompras.encriptacion;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.PrivateKey;
+import java.security.Signature;
+
+public class Firma {
+	
+	//	Este metodo recibe la ruta del archivo a firmar,la clave privada,y la ruta donde se va a guardar la firma
+	
+	public void firmarTramite(String rutaTramite,PrivateKey clavePrivada,String ruta )  throws Exception {
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
