a5c617f0bfe3116221b5df3f9c4974793acaf788
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2017-11-30 18:31:36 -0600

    .

--- src/main/java/com/sitemascompras/encriptacion/Firma.java
+++ src/main/java/com/sitemascompras/encriptacion/Firma.java
@@ -11,12 +11,30 @@ public class Firma {
 	
 	public void firmarTramite(String rutaTramite,PrivateKey clavePrivada,String ruta )  throws Exception {
 		
+	public void firmarTramite(String rutaTramite,PrivateKey clavePrivada,String rutafirma )  throws Exception {
 		
+		byte[] data = leerArchivo(rutaTramite);
+		Signature dsa = Signature.getInstance("SHA1withRSA"); 
+		dsa.initSign(clavePrivada);
+		dsa.update(data);
+		byte[] firma = dsa.sign();
 		
+		guardarFirma(rutafirma, firma);
+	}
+	
+	public byte[] leerArchivo(String ruta) throws Exception {
 		
+		return Files.readAllBytes(Paths.get(ruta));
 	}
 	
 	
 	
+	public void guardarFirma(String rutafirma, byte[] firma) throws Exception {
+		
 
+		FileOutputStream fos = new FileOutputStream(rutafirma);
+		fos.write(firma);
+		fos.close();
+		
+	}
 }
