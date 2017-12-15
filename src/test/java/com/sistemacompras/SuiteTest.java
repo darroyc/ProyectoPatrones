4def34c75361ef5ad36a2ad882fe9fa8ceef21bc
hayleen bonilla <hbonillab@ucenfotec.ac.cr>
2017-12-14 23:43:25 -0600

    MTramite test
    Crear la clase MTramiteTest y agregar MTramiteTest a SuiteTest

--- src/test/java/com/sistemacompras/SuiteTest.java
+++ src/test/java/com/sistemacompras/SuiteTest.java
@@ -3,9 +3,13 @@ package com.sistemacompras;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.testsMultis.MDepartamentoTest;
+import com.testsMultis.MTramiteTest;
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    MDepartamentoTest.class
+    MDepartamentoTest.class,
+    MTramiteTest.class
 })
 
 public class SuiteTest {
--- src/test/java/com/testsMultis/MDepartamentoTest.java
+++ src/test/java/com/testsMultis/MDepartamentoTest.java
@@ -1,4 +1,4 @@
-package com.sistemacompras;
+package com.testsMultis;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sistemacompras.multis.MDepartamento;
+import com.sistemacompras.objects.Departamento;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,6 +23,26 @@ public class MDepartamentoTest {
 	@Test
 	public void testCrearDep() throws Exception{
 		dep.crearDepartamento("Contaduria");
+	}
+	
+	@Test
+	public void testBuscarDep() throws Exception{
+		dep.buscarDepartamentoPorId(1);
+	}
+	
+	@Test
+	public void testBuscarDepPorNombre() throws Exception{
+		dep.buscarDepartamentoPorNombre("IT");
+	}
+	
+	@Test
+	public void testBuscarDepS() throws Exception{
+		dep.buscarDepartamentos();
+	}
+	
+	@Test
+	public void testBuscarEmpPorDep() throws Exception{
+		dep.buscarEmpleadosPorDepartamento(1);
 	}
 	
 }
