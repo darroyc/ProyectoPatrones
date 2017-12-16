5ea7db1236873cac98dbc77a4e50ed150f1748c8
CamaxUni <33705060+CamaxUni@users.noreply.github.com>
2017-12-15 20:21:49 -0600

    menu tramite
    menu IT permite la creacion de un tramite

--- src/main/java/com/sistemacompras/menus/MenuIT.java
+++ src/main/java/com/sistemacompras/menus/MenuIT.java
@@ -1,6 +1,8 @@
 package com.sistemacompras.menus;
 
 import com.sistemacompras.enums.Departamentos;
+import com.sistemacompras.multis.*;
+import com.sistemacompras.objects.Tramite;
 
 public class MenuIT extends Menu{
 
@@ -11,7 +13,7 @@ public class MenuIT extends Menu{
     	String dato = "";
     	
     	do {
-    		System.out.println("Opcion1");
+    		System.out.println("Crear Tramite");
     		System.out.println("Opcion1");
     		System.out.println("Opcion1");
     		System.out.println("Opcion1");
@@ -20,8 +22,40 @@ public class MenuIT extends Menu{
     		opc = Integer.parseInt(dato);
     		
     		switch(opc) {
-    		
+
     		case 1:
+    			
+    			int idTramite;
+    			String nombreTramite = "";
+    			String descripcionTramite = "";
+    			String contenidoTramite = "";
+    			String firmaDigTramite = "";
+    			String origenTramite = "";
+    			String destinoTramite = "";
+    			
+    			System.out.println("Deme el ID de su tramite");
+        		dato = in.readLine();
+        		idTramite = Integer.parseInt(dato);
+        		
+        		System.out.println("Deme el nombre de su tramite");
+        		nombreTramite = in.readLine();
+        		
+        		System.out.println("Describa al tramite.");
+        		descripcionTramite = in.readLine();
+        		
+        		System.out.println("Ingrese textualmente el contenido del traminte.");
+        		contenidoTramite = in.readLine();
+        		
+        		System.out.println("Ingrese su firma Digital");
+        		firmaDigTramite = in.readLine();
+        		
+        		System.out.println("Digite el origen del tramite.");
+        		origenTramite = in.readLine();
+        		
+        		System.out.println("Digite el destino del traminte");
+        		destinoTramite = in.readLine();
+
+    			Tramite tramite = new Tramite(idTramite, nombreTramite, descripcionTramite, contenidoTramite, firmaDigTramite, origenTramite, destinoTramite);
     			break;
     		
     		case 2:
@@ -43,11 +77,11 @@ public class MenuIT extends Menu{
 //        print("\nIngrese su contrasenia");
 //        pass = in.readLine();
         
-        opcion = GESTOR.login(id, pass);
-        if(!opcion.equals(null)){
-        	FabricaMenu.crearNuevoMenu(Departamentos.values()[opcion-1]);
-        }else{
-        	login();
-        }
+//        opcion = GESTOR.login(id, pass);
+//        if(!opcion.equals(null)){
+//        	FabricaMenu.crearNuevoMenu(Departamentos.values()[opcion-1]);
+//        }else{
+//        	login();
+//        }
     }
 }
--- src/main/java/com/sistemacompras/multis/MTramite.java
+++ src/main/java/com/sistemacompras/multis/MTramite.java
@@ -14,7 +14,12 @@ public class MTramite {
 	 MDepartamento buscardepartamento = new MDepartamento();
 	 Departamento departamento;
 	 
-    public void crearTramite(String nombreTramite,String descripcionTramite,String contenidoTramite,String firmaDigital,String origen,String destino) throws Exception{
+	
+	 
+    public MTramite() {
+	}
+
+	public void crearTramite(String nombreTramite,String descripcionTramite,String contenidoTramite,String firmaDigital,String origen,String destino) throws Exception{
         String sql;
         String llavePublica;
         String mensajeEncriptado;
